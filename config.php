6c456356b7704fc6cf15e961e239f71676b846cd
chwqhd <chwqhd@126.com>
2017-03-06 15:20:31 +0800

    修改index.php 添加 config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'DB_TYPE'   => 'mysql',
+    'DB_HOST'   => 'localhost',
+    'DB_USER'   => 'root',
+    'DB_PWD'   => '7658032',
+    'DB_NAME'   => 'ectouch',
+    'DB_PREFIX'   => 'ecs_',
+    'DB_PORT'   => '3306',
+    'DB_CHARSET'   => 'utf8',
+);
